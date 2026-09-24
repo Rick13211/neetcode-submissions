e4fbebc13d6483bbf4e07a9252336136715aba12
Rick13211 <rick13211roy@gmail.com>
2026-09-24 11:40:42 +0530

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.py
@@ -0,0 +1,53 @@
+class ListNode:
+    def __init__(self, key: int = 0, val: int = 0):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.head = ListNode()
+        self.tail = ListNode()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node: ListNode) -> None:
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+    def _add_to_tail(self, node: ListNode) -> None:
+        last_node = self.tail.prev
+        
+        last_node.next = node
+        node.prev = last_node
+        node.next = self.tail
+        self.tail.prev = node
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add_to_tail(node)
+            return node.val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            node.val = value
+            self._remove(node)
+            self._add_to_tail(node)
+        else:
+            if len(self.cache) >= self.capacity:
+                lru_node = self.head.next
+                self._remove(lru_node)
+                del self.cache[lru_node.key]
+
+            new_node = ListNode(key, value)
+            self.cache[key] = new_node
+            self._add_to_tail(new_node)
